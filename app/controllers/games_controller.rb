a9a69b80de647b5efd1444f17feaaf8d9ce94d8d
denio-rus <denio@yandex.ru>
2026-06-21 14:37:58 +0700

    First MVP
    one game - Reaction Time(works with data from GameStrategy, works on stimulus(JS + html view)
    save GS

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,15 @@
+require "ostruct"
+
 class GamesController < ApplicationController
   def index
     @games = Game.all
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  def play
+    redirect_to game_game_sessions_path(game_id: params[:id])
   end
 end
