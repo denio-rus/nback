23cfba35c5db89781dd83717b519eed0b03af48b
denio-rus <denio@yandex.ru>
2026-06-24 19:19:29 +0700

    Add registration flow

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root to: "games#index"
 
   resource :session
+  resources :registrations, only: [ :new, :create ]
   resources :passwords, param: :token
+
+  resources :users, only: [ :show, :edit, :update ]
+
   namespace :admin do
     resources :users
   end
